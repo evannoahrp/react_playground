c8ab2f60c8d83e107ef392ab9cfb75b61f5c3c81
Evannoah <evannoah11@gmail.com>
2022-04-22 06:18:33 +0700

    update task

--- src/App.js
+++ src/App.js
@@ -23,10 +23,16 @@ function App() {
       reminder: false
     }
   ])
+
+// deletetassk
+const deleteTask = (id) => {
+  console.log('delete', id)
+} 
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
